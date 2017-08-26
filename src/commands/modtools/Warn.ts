a601dbb0c53358ac608e9787f3fa9a32616bb5b6
Mike Wohlrab <Mike@Layer7.Solutions>
2017-08-26 07:49:13 -0500

    Added template for Warn command. Updated deps.
    **Warn:**
    
    - The warn command can now use optional note argument `clan` to have it warn use with pre-defined clan warning text. The word `clan` must be the only note keyword. Usage: `.warn User clan`.
    
    **General:**
    
    - Updated deps

--- src/commands/modtools/Warn.ts
+++ src/commands/modtools/Warn.ts
@@ -81,7 +81,11 @@ export default class Mute extends Command<SweeperClient> {
 			// Set note info
 			let note: string = '';
 			note = this.parseNote(args);
-			if (note.length === 0) { note = 'Please be kind to each other and read our rules in #rules-and-info.'; }
+			if (note.length === 0) {
+				note = 'Please be kind to each other and read our rules in #rules-and-info.';
+			} else if (note.toLowerCase() === 'clan') {
+				note = 'Regarding your promotion in #clan-recruitment, please note that the channel rules ask that you be an active member of the server. Please interact with and get to know some of the other folks around here and participate in some discussions over the next few days, then feel free to post again once you\'re a bit more active.';
+			}
 
 			if (user.id === message.author.id || user.id === message.guild.ownerID || user.bot) {
 				message.channel.send('You may not use this command on that user.');
